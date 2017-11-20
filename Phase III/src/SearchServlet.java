5dc3923eda0dd86fc52fc2c30c27314f7f48495e
Aakash Sinha <aakash.sinha@st.niituniversity.in>
2017-11-20 22:08:13 +0530

    added searchServlet.java

---
+++ Phase III/src/SearchServlet.java
@@ -0,0 +1,32 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import esearch.SearchClient;
+
+/**
+ * Servlet implementation class SearchServlet
+ */
+@WebServlet("/SearchServlet")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		
+//		String query = request.getParameter("query");
+//		System.out.println("q:" + query);
+//		String output = SearchClient.searchDoc(query);
+//		System.out.println(output);
+		response.sendRedirect("search.jsp");
+	}
+
+}
